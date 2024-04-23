fcabd1eb1117678e2bf4324cd70225f115a3e417
Alice Ribeiro <aliceribeiro.work@gmail.com>
2024-04-22 23:46:20 -0300

    feat: adiciona arquivos em php

---
+++ resources/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Course: PHP</title>
+</head>
+<body>
+    <header class="header">
+        <h1>Curso PHP</h1>
+        <h2>Índice dos Exercícios</h2>
+    </header>
+    <main class="main">
+        <div class="content">
+            <nav class="modules">
+                <div class="module green">
+                    <h3>Módulo 01</h3>
+                    <ul>
+                        <li>
+                            <a href="exercise.php?dir=basic&file=hello">
+                                Olá PHP
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </main>
+    <footer class="footer">
+        COD3R & ALUNOS © <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
