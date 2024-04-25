7efe9a3dbf5754c3303af8bfb7f32b685f081bbf
Alice Ribeiro <aliceribeiro.work@gmail.com>
2024-04-24 23:01:15 -0300

    feat: adiciona teste do tipo int

---
+++ type/int.php
@@ -0,0 +1,24 @@
+<div class="title">Tipo Inteiro</div>
+
+<?php
+
+echo 11; // Exibe o número inteiro 11
+echo '<br>';
+
+var_dump(11); // Exibe informações sobre a variável, incluindo seu tipo e valor     
+echo '<br>'; 
+
+echo PHP_INT_MAX; // Exibe o valor máximo permitido para um inteiro no PHP
+echo '<br>';
+
+echo PHP_INT_MIN; // Exibe o valor mínimo permitido para um inteiro no PHP
+echo '<br>';
+
+echo 011; // Exibe o valor 11 na base octal (equivalente a 9 em decimal)
+echo '<br>'; 
+
+echo 0b11000111001; // Exibe o valor binário correspondente a 0b11000111001 (equivalente a 1561 em decimal)
+echo '<br>'; 
+
+echo 0x11; // Exibe o valor hexadecimal correspondente a 0x11 (equivalente a 17 em decimal)
+echo '<br>'; 
