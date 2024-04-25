21fe71b520aa5d55b7734042c0fcd158a924ea3d
Alice Ribeiro <aliceribeiro.work@gmail.com>
2024-04-24 23:01:30 -0300

    feat: adiciona teste do tipo float

---
+++ type/float.php
@@ -0,0 +1,27 @@
+<div class="title">Tipo Inteiro</div>
+
+<?php
+
+// Exibe o número decimal 1.1
+echo 1.1 ;
+echo '<br>'; 
+
+// Exibe informações sobre a variável, incluindo seu tipo e valor (1.0 é considerado um float)
+var_dump(1.0);
+echo '<br>'; 
+
+// Exibe o valor máximo permitido para um float no PHP
+echo PHP_FLOAT_MAX;
+echo '<br>';
+
+// Exibe o valor mínimo permitido para um float positivo no PHP
+echo PHP_FLOAT_MIN;
+echo '<br>'; 
+
+// Exibe o valor 1.2 multiplicado por 10 elevado à potência de 3 (1200)
+echo 1.2e3 ;
+echo '<br>'; 
+
+// Exibe o valor 13 multiplicado por 10 elevado à potência de -3 (0.013)
+echo 13e-3;
+echo '<br>'; 
