9a0310c81e1c6c8b51c1578f8d65ac7c98ddaa7d
Alice Ribeiro <aliceribeiro.work@gmail.com>
2024-04-25 22:35:20 -0300

    feat: adiciona teste de operações aritimeticas

---
+++ type/arithmetic.php
@@ -0,0 +1,57 @@
+<div class="title">Operações Aritiméticas</div>
+
+<?php 
+
+echo 1 + 1;
+echo '<br>';
+var_dump(1+1);
+echo '<br>';
+
+
+echo 1 + 2.5;
+echo '<br>';
+
+echo 10 - 2; 
+echo '<br>';
+
+echo 2 * 5;
+echo '<br>';
+
+echo 7 / 4;
+echo '<br>';
+
+echo intdiv(7,4);
+echo '<br>';
+
+echo round(7/4);
+echo '<br>';
+
+echo 7 % 4;
+echo '<br>';
+
+echo 7 % 2;
+echo '<br>';
+
+echo 8 % 2;
+echo '<br>';
+
+echo 7 / 0;
+echo '<br>';
+
+echo intdiv(7,0);
+echo '<br>';
+
+echo 4 ** 2;
+echo '<br>';
+
+echo 2 + 3 * 4;
+echo '<br>';
+
+echo (2 + 3) * 4;
+echo '<br>';
+
+echo 2 + 3 * 4 ** 2;
+echo '<br>';
+
+echo ((2 + 3) * 4) ** 2;
+echo '<br>';
